3c0aa7b3120049aee9d7c00cfffedb714e832eaf
chillyweather <d.dzmitryeu@gmail.com>
2022-01-25 11:13:08 +0200

    About component structure

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <section className="about">
+      <div className="about__container">
+        <div className="avatar" />
+        <div className="about__info-container">
+          <div className="about__info-title">About me</div>
+          <div className="about__info-content">Lorem Ipsum</div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;

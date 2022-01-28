004c3bc6cf33183ee49280291b4b19e84efef24f
chillyweather <d.dzmitryeu@gmail.com>
2022-01-28 13:30:02 +0200

    NotFoud section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import About from '../About/About';
 import SearchForm from '../SearchForm/SearchForm';
 import NewsCardList from '../NewsCardList/NewsCardList';
+import NotFound from '../NotFound/NotFound';
 
 function Main() {
   return (
     <div className="Main">
       <SearchForm />
+      <NotFound />
       <NewsCardList />
       <About />
     </div>
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+function NotFound() {
+  return (
+    <section className="notFound">
+      <div className="notFound__icon" />
+      <h2 className="notFound__title">Nothing found</h2>
+      <p className="notFound__text">Sorry, but nothing matched your search terms.</p>
+    </section>
+  );
+}
+
+export default NotFound;
